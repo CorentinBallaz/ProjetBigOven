0fd7098971ab1f9e676876c845ee875e7d422a65
TanguyMrtn <tanguy.mariton38@gmail.com>
2019-10-16 16:27:15 +0200

    harvesting data and data added

---
+++ python/harvestData.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 16 11:16:20 2019
+
+@author: tangu
+"""
+
+import requests
+import json
+
+
+########################  HARVEST DATA ONLINE AND SAVE THEM IN JSON  ########################
+
+#r = requests.get("https://www.food2fork.com/api/search?key=91d2b2b099e5b421ee30ce2eeb65b270") 
+#
+#data = r.json()
+#
+#with open('data.json', 'w', encoding='utf-8') as f:
+#    json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+########################  LOAD DATA FROM JSON FILE  ########################
+
+with open('data.json') as json_file:
+    data = json.load(json_file)
+
+recipes = data['recipes']
+
+final_data = []
+
+for recipe in recipes :
+    recipe_id= recipe['recipe_id']
+    
+    url = 'https://www.food2fork.com/api/get?key=91d2b2b099e5b421ee30ce2eeb65b270&rId='+recipe_id
+    
+    r = requests.get(url)
+    
+    current_data = r.json()
+    
+    final_data.append(current_data)
+
+with open('recipes.json','w',encoding='utf-8') as f :
+    json.dump(final_data,f,ensure_ascii=False,indent=4)
+
