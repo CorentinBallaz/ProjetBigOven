a841776230c13d0842a0f20b491375085cfe3fed
WilliamBurillon <William.Burillon@etu.univ-savoie.fr>
2019-10-21 17:18:52 +0200

    Api get (all and one Recipe) added

--- F2F/FoodProject/api/MyApi/routes/index.js
+++ F2F/FoodProject/api/MyApi/routes/index.js
@@ -1,55 +1,66 @@
 const router = require('express').Router();
 const controller = require('../controllers')
-router.get('/api/todos',(req,res)=>{
-    controller.getAllTodos(req,res);
-    // res.send('get todos');
-});
+// router.get('/api/todos',(req,res)=>{
+//     controller.getAllTodos(req,res);
+//     // res.send('get todos');
+// });
+//
+// router.get('/api/createTodo',(req,res)=>{
+//     controller.getAllTodos(req,res);
+//
+//     // res.send('get todos');
+// });
+//
+// router.get('/api/todo/:id',(req,res)=>{
+//     // controller.getTodo(res,res);
+//     controller.getOneTodo(req,res);
+//     // res.send(req.params.id);
+// });
+// router.post('/api/todo',(req,res)=>{
+//     // res.send('post a )
+//     controller.createTodo(req,res);
+// });
+// router.put('/api/todo/:id',(req,res)=>{
+//     // res.send('update a todo');
+//     controller.modifyTodo(req,res);
+//    // controller.putTodo(req,res);
+// });
+// router.delete('/api/delete/todo/:id',(req,res)=>{
+//     // res.send('delete a todo');
+//     controller.deleteTodo(req,res);
+// });
+// // Passe done a true
+// router.put('/api/todo/done/:id',(req,res)=>{
+//     controller.doneTodo(req,res);
+// });
+//
+//
+// router.get('/api/delete/todo/:id',(req,res)=>{
+//     // res.send('delete a todo');
+//     controller.deleteTodo(req,res);
+// });
+//
+// router.put('/api/todo/notdone/:id',(req,res)=>{
+//     controller.notDoneTodo(req,res);
+// });
+//
+// router.get('/api/todos/done',(req,res)=>{
+//     controller.getTrue(req,res);
+// });
+// router.get('/api/todos/current',(req,res)=>{
+//     controller.getFalse(req,res);
+// });
+//
 
-router.get('/api/createTodo',(req,res)=>{
-    controller.getAllTodos(req,res);
+router.get('/MyApi/recipes/',(req,res)=>{
 
-    // res.send('get todos');
+    // res.send('Express response');
+    controller.getAllRecipes(req,res);
 });
+router.get('/MyApi/recipe/:id',(req,res)=>{
 
-router.get('/api/todo/:id',(req,res)=>{
-    // controller.getTodo(res,res);
-    controller.getOneTodo(req,res);
-    // res.send(req.params.id);
+    // res.send('Express response');
+    controller.getRecipe(req,res);
 });
-router.post('/api/todo',(req,res)=>{
-    // res.send('post a )
-    controller.createTodo(req,res);
-});
-router.put('/api/todo/:id',(req,res)=>{
-    // res.send('update a todo');
-    controller.modifyTodo(req,res);
-   // controller.putTodo(req,res);
-});
-router.delete('/api/delete/todo/:id',(req,res)=>{
-    // res.send('delete a todo');
-    controller.deleteTodo(req,res);
-});
-// Passe done a true
-router.put('/api/todo/done/:id',(req,res)=>{
-    controller.doneTodo(req,res);
-});
-
-
-router.get('/api/delete/todo/:id',(req,res)=>{
-    // res.send('delete a todo');
-    controller.deleteTodo(req,res);
-});
-
-router.put('/api/todo/notdone/:id',(req,res)=>{
-    controller.notDoneTodo(req,res);
-});
-
-router.get('/api/todos/done',(req,res)=>{
-    controller.getTrue(req,res);
-});
-router.get('/api/todos/current',(req,res)=>{
-    controller.getFalse(req,res);
-});
-
 
 module.exports=router;
