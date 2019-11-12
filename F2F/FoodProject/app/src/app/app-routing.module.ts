11bad3068c73f70d865d21988a00901897f0d025
WilliamBurillon <William.Burillon@etu.univ-savoie.fr>
2019-11-12 17:15:36 +0100

    get 30 random recipies + 30 with query recipies + get by id

--- F2F/FoodProject/app/src/app/app-routing.module.ts
+++ F2F/FoodProject/app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'research', loadChildren: './research/research.module#ResearchPageModule' }
 ];
 
 @NgModule({
---
+++ F2F/FoodProject/app/src/app/research/research.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ResearchPage } from './research.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ResearchPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ResearchPage]
+})
+export class ResearchPageModule {}
