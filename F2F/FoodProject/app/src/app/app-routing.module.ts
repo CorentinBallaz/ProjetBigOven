972ebd26ff64204684e543e4cef5d9246d53dc44
CorentinBallaz <corentin.ballaz@yahoo.fr>
2019-12-02 15:51:07 +0100

    ajout panier

--- F2F/FoodProject/app/src/app/app-routing.module.ts
+++ F2F/FoodProject/app/src/app/app-routing.module.ts
@@ -5,7 +5,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'details/:id', loadChildren: './details/details.module#DetailsPageModule' },  { path: 'cart', loadChildren: './cart/cart.module#CartPageModule' },
+  { path: 'details/:id', loadChildren: './details/details.module#DetailsPageModule' },
+  { path: 'cart', loadChildren: './cart/cart.module#CartPageModule' },
+  { path: 'favoris', loadChildren: './favoris/favoris.module#FavorisPageModule' },
 
 
 ];
---
+++ F2F/FoodProject/app/src/app/favoris/favoris.page.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { RestService } from '../rest.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+@Component({
+  selector: 'app-favoris',
+  templateUrl: './favoris.page.html',
+  styleUrls: ['./favoris.page.scss'],
+})
+
+export class FavorisPage implements OnInit {
+  api : RestService;
+  publisher : string;
+  recipeNames : any;
+  myImage: string;
+  isIndeterminate:boolean;
+  masterCheck:boolean;
+
+  constructor(public restapi: RestService, public loadingController: LoadingController, private route: ActivatedRoute) {
+
+    this.api = restapi;
+
+  }
+
+async getAllRecipes() {
+	const loading = await this.loadingController.create({
+      message: 'Loading'
+    });
+    await loading.present();
+
+    await this.api.getRecipes().subscribe(res => {
+    console.log(res[0]);
+    this.recipeNames = [];
+    for (var j = 0; j < res.length; j++) {
+    	var currentRecipeName = res[j].recipe.title;
+    	var currentImage = res[j].recipe.image_url;
+    	var currentId = res[j]._id;
+    	console.log(currentId);
+    	var currentJsonRecipeName = {name:currentRecipeName, image:currentImage, id:currentId};
+    	this.recipeNames.push(currentJsonRecipeName);
+    }
+    console.log(this.recipeNames);
+    loading.dismiss();
+    },err => {
+    	console.log(err);
+    	loading.dismiss();
+    });
+}
+
+  ngOnInit() {
+    this.getAllRecipes();
+  }
+
+}
