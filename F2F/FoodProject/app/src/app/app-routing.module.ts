8952a0befeae0f1b7acce9468f02ae7b877dad84
CorentinBallaz <corentin.ballaz@yahoo.fr>
2019-12-02 15:05:26 +0100

    ajout des pages favoris

--- F2F/FoodProject/app/src/app/app-routing.module.ts
+++ F2F/FoodProject/app/src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'details/:id', loadChildren: './details/details.module#DetailsPageModule' },
+  { path: 'favoris', loadChildren: './favoris/favoris.module#FavorisPageModule' },
+
 
 ];
 
